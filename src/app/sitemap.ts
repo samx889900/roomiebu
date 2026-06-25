cd38c7d0bd7b11648b5d734565f0070eb0b06a6b
thevikramrajput <vikrammadhad@gmail.com>
2026-06-25 21:57:10 +0530

    Update sitemap with buconfess.in link

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -34,5 +34,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.5,
     },
+    {
+      url: "https://buconfess.in",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
   ];
 }
