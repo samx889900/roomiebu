f476649fef2202cefa8a90b3fc4b9146e8294ae3
thevikramrajput <vikrammadhad@gmail.com>
2026-06-27 10:58:01 +0530

    Fix sitemap external domain issue

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -34,11 +34,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.5,
     },
-    {
-      url: "https://buconfess.in",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
   ];
 }
